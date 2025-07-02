88c2b440f97d2bc0fca4acaa29a1e4a40d6bd627
agamlatiff <agam.latiff@gmail.com>
2025-07-02 15:28:00 +0700

    change image

--- data/blogs.tsx
+++ data/blogs.tsx
@@ -5,19 +5,19 @@ export const blog: Blog[] = [
   {
     title: "The Art of Designing Timeless Masterpieces",
     description: "Dive into the realm of limitless creativity as we explore the techniques and inspirations behind crafting visually stunning and timeless designs that captivate hearts and minds.",
-    image: "/assets/blog-1.png",
+    image: "/assets/bloglist-1.jpg",
     tags: ["Slate", "Contrux"],
   },
   {
     title: "Stay Ahead of the Curve in the Visual World",
     description: "Discover the secrets of designing impactful brand experiences that leave a lasting impression on your audience, forging deep connections and driving brand loyalty.",
-    image: "/assets/blog-2.png",
+    image: "/assets/bloglist-1.jpg",
     tags: ["Slate", "Contrux"],
   },
   {
     title: "Crafting Emotionally Engaging User Experiences",
     description: "Join us on a journey of exploration as we push the boundaries of design, unveiling cutting-edge concepts and techniques that challenge conventional norms and redefine what's possible.",
-    image: "/assets/blog-3.png",
+    image: "/assets/bloglist-1.jpg",
     tags: ["Slate", "Contrux"],
   },
 ];
